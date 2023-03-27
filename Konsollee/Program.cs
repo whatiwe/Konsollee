1e9c45308e0c8f055c8b4306bfce8d2ef6af86ce
bulatezkay <bulatezkay_now@mail.ru>
2023-03-27 11:09:10 +0500

    Добавьте файлы проекта.

---
+++ Konsollee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Konsollee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добрый день группа 2024 и 2024к");
+        }
+    }
+}
